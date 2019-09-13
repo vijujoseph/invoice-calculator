645fcddcbf9d33cb1e5204cc495ed6ffe88ad25d
Joseph, Viju <vijujoseph@gmail.com>
2019-09-13 18:16:15 +0200

    Modifying the title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Byrd Invoice Calculator';
+  title = 'Invoice Calculator Service';
 }
